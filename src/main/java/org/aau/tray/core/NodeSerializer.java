f54a7b4b458b18145a247134d041f4f3fe5d183a
MrPekar <martin.pekar@outlook.dk>
2022-02-13 13:54:47 +0100

    Node serialization

--- src/main/java/org/aau/tray/core/NodeSerializer.java
+++ src/main/java/org/aau/tray/core/NodeSerializer.java
@@ -1,6 +1,9 @@
 package org.aau.tray.core;
 
+import org.apache.commons.lang3.SerializationException;
+import org.apache.jena.graph.BlankNodeId;
 import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
 
 import java.io.Serializable;
 
@@ -15,7 +18,7 @@ public class NodeSerializer implements Serializable
 
     public NodeSerializer(String serialized)
     {
-
+        this.n = deserialize(serialized);
     }
 
     public Node getNode()
@@ -25,11 +28,14 @@ public class NodeSerializer implements Serializable
 
     public String serialize()
     {
+        if (this.n == null)
+            throw new NullPointerException("Node is null (maybe because it could not be de-serialized)");
+
         if (n.isBlank())
             return n.getBlankNodeId().toString() + ":BLANK";
 
         else if (n.isLiteral())
-            return n.getLiteral().toString() + ":LITERAL";
+            return n.getLiteral().getValue().toString() + ":LITERAL";
 
         else if (n.isURI())
             return n.getURI() + ":URI";
@@ -41,8 +47,26 @@ public class NodeSerializer implements Serializable
             return n.toString() + ":OTHER";
     }
 
-    private static void deserialize(String serialized)
+    private static Node deserialize(String serialized)
     {
+        String[] split = serialized.split(":");
 
+        if (split.length != 2)
+            throw new SerializationException("Serialized node does not have a single ':' split");
+
+        if (split[1].equals("BLANK"))
+            return NodeFactory.createBlankNode(BlankNodeId.create(split[0]));
+
+        else if (split[1].equals("LITERAL"))
+            return NodeFactory.createLiteral(split[0]);
+
+        else if (split[1].equals("URI"))
+            return NodeFactory.createURI(split[0]);
+
+        else if (split[1].equals("VARIABLE"))
+            return NodeFactory.createVariable(split[0]);
+
+        else
+            return null;
     }
 }
--- src/test/java/org/aau/tray/core/NodeSerializerTest.java
+++ src/test/java/org/aau/tray/core/NodeSerializerTest.java
@@ -1,5 +1,32 @@
 package org.aau.tray.core;
 
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NodeSerializerTest
 {
+    @Test
+    public void testSerialization()
+    {
+        Node var = NodeFactory.createVariable("test"), uri = NodeFactory.createURI("uri.test"),
+                blank = NodeFactory.createBlankNode("blank"), literal = NodeFactory.createLiteral("12345");
+
+        assertEquals("test:VARIABLE", (new NodeSerializer(var)).serialize());
+        assertEquals("uri.test:URI", (new NodeSerializer(uri)).serialize());
+        assertEquals("blank:BLANK", (new NodeSerializer(blank)).serialize());
+        assertEquals("12345:LITERAL", (new NodeSerializer(literal)).serialize());
+    }
+
+    @Test
+    public void testDeserialization()
+    {
+        String var = "test:VARIABLE", uri = "uri.test:URI", blank = "blank:BLANK", literal = "12345:LITERAL";
+        assertEquals(NodeFactory.createVariable("test"), (new NodeSerializer(var)).getNode());
+        assertEquals(NodeFactory.createURI("uri.test"), (new NodeSerializer(uri)).getNode());
+        assertEquals(NodeFactory.createBlankNode("blank"), (new NodeSerializer(blank)).getNode());
+        assertEquals(NodeFactory.createLiteral("12345"), (new NodeSerializer(literal)).getNode());
+    }
 }
